354454a119e84165efaf2387f8daf19ddc6d25ef
lion-ilustre <74267850+lionilustres@users.noreply.github.com>
2025-02-01 17:13:59 -0500

    new_update

---
+++ netlify/functions/mercadopago.js
@@ -0,0 +1,25 @@
+const mercadopago = require('mercadopago');
+const axios = require('axios');
+
+exports.handler = async (event, context) => {
+  try {
+    console.log("Función iniciada."); // Mensaje de prueba
+
+        mercadopago.configure({
+            access_token: process.env.MERCADO_PAGO_ACCESS_TOKEN
+        })
+
+        console.log("Mercado Pago configurado")
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "¡Hola desde mi función!" }),
+    };
+  } catch (error) {
+    console.error("Error en la función:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
